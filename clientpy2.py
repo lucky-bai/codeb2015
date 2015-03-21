f820ae21d98164d8c3deaada863075ba869a5fa7
luckyt <bai.li.2005@gmail.com>
2015-03-20 20:22:33 -0400

    initial commit

---
+++ clientpy2.py
@@ -0,0 +1,59 @@
+import socket
+import sys
+  
+def run(*commands):
+  HOST, PORT = "codebb.cloudapp.net", 17429
+  
+  data=OUR_USERNAME + " " + OUR_PASSWORD + "\n" + "\n".join(commands) + "\nCLOSE_CONNECTION\n"
+  return_lines = []
+
+  try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    sock.connect((HOST, PORT))
+    sock.sendall(data)
+    sfile = sock.makefile()
+    rline = sfile.readline()
+    while rline:
+      #print(rline.strip())
+      return_lines.append(rline.strip())
+      rline = sfile.readline()
+  finally:
+    sock.close()
+
+  return return_lines
+
+def subscribe():
+  HOST, PORT = "codebb.cloudapp.net", 17429
+  
+  data=OUR_USERNAME + " " + OUR_PASSWORD + "\nSUBSCRIBE\n"
+
+  try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    sock.connect((HOST, PORT))
+    sock.sendall(data)
+    sfile = sock.makefile()
+    rline = sfile.readline()
+    while rline:
+      print(rline.strip())
+      rline = sfile.readline()
+  finally:
+    sock.close()
+
+
+OUR_USERNAME = "/dev/rand_AB"
+OUR_PASSWORD = "pi<4"
+
+def get_securities():
+  inp = run("SECURITIES")[0].split()[1:]
+  out = []
+  for i in range(len(inp)/4):
+    out.append( (inp[4*i], float(inp[4*i+1]), float(inp[4*i+2]), float(inp[4*i+3])) )
+
+  for o in out:
+    print o
+
+get_securities()
+
+
