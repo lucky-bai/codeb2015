9046834442c800f2e577d41f82e7159040d94441
luckyt <bai.li.2005@gmail.com>
2015-03-20 21:35:28 -0400

    can read remote data into local

--- clientpy2.py
+++ clientpy2.py
@@ -45,15 +45,45 @@ def subscribe():
 OUR_USERNAME = "/dev/rand_AB"
 OUR_PASSWORD = "pi<4"
 
+securities = {}
+my_cash = 0
+my_securities = {}
+my_orders = {}
+
+
+# Query all the securities
 def get_securities():
   inp = run("SECURITIES")[0].split()[1:]
-  out = []
   for i in range(len(inp)/4):
-    out.append( (inp[4*i], float(inp[4*i+1]), float(inp[4*i+2]), float(inp[4*i+3])) )
+    securities[inp[4*i]] = (float(inp[4*i+1]), float(inp[4*i+2]), float(inp[4*i+3]))
 
-  for o in out:
-    print o
+def get_cash():
+  global my_cash
+  my_cash = float(run("MY_CASH")[0].split()[1])
+
+def get_my_securities():
+  inp = run("MY_SECURITIES")[0].split()[1:]
+  for i in range(len(inp)/3):
+    my_securities[inp[3*i]] = (float(inp[3*i+1]), float(inp[3*i+2]))
+
+def get_my_orders():
+  global my_orders
+  my_orders = {}
+  inp = run("MY_ORDERS")[0].split()[1:]
+  for i in range(len(inp)/4):
+    my_orders[inp[4*i+1]] = (inp[4*i], float(inp[4*i+2]), float(inp[4*i+3]))
+  
+
 
 get_securities()
+get_cash()
+get_my_securities()
+get_my_orders()
+
+print securities
+print my_cash
+print my_securities
+print my_orders
+
 
 
